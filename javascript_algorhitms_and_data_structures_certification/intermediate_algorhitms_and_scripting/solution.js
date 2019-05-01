8fd0f46778eafd70bdee5389f01176f23a550983
krutijan1 <krutinajan@gmail.com>
2019-05-02 00:04:02 +0200

    add solution

---
+++ javascript_algorhitms_and_data_structures_certification/intermediate_algorhitms_and_scripting/solution.js
@@ -0,0 +1,22 @@
+function isPrimeNumber(number) {
+    let limit = number >> 1;
+    for (let i = 2; i <= limit; i++) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function sumPrimes(num) {
+    let sum = 0;
+    for (let i = 2; i <= num; i++) {
+        if (isPrimeNumber(i)) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+sumPrimes(10);
